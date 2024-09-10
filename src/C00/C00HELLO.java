4f61cfeb61892a4edf4a74cbf3ab7e49ace4475e
junyoung6 <rlrpqkd12@naver.com>
2024-09-10 17:40:43 +0900

    1Day

--- src/C00/C00HELLO.java
+++ src/C00/C00HELLO.java
@@ -1,10 +1,17 @@
 package C00;
 
-public class C00HELLO {
+public class C00HELLO  //클래스 영역 - 객체지향 문법 적용단위
+{	
+	// 메서드(함수) : 작업을 수행하는데 필요한 여러 코드를 묶어서 실행하는 단위
+	// main 메서드 : 최초 실행 함수
+	// library 메서드: 미리 만들어져 제공되어지는 메서드
+	// 사용자의 정의 메서드: 개발자에 의해 만들어지는 메서드
 
-	public static void main(String[] args) {
+	public static void main(String[] args) // 메서드 영역 - 절차지향 문법 적용 단위
+	{
 		// TODO Auto-generated method stub
-
+		System.out.println("라이브러리 메서드 호출 \n 입니다."); // 라이브러리 메서드 호출 
+		
 	}
 
 }
---
+++ src/C01/C01SystemOut.java
@@ -0,0 +1,38 @@
+package C01;
+
+public class C01SystemOut {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// System.out.print()
+		// ESCAPE문자:
+		// 1. \n : 개행, 줄바꿈
+		// 2. \b : 백스페이스
+		// 3. \t : 탭길이(기본 8칸) 만큼 커서 이동
+		
+		
+		
+		
+//		System.out.println("HELLO WORLD");
+//		System.out.println("Hello World");
+//		System.out.print("Hello World");
+//		System.out.println("Hello\t World");
+//		
+//		
+//		//서식문자
+//		System.out.printf("%d %d %d \n",10,20,30); // 정수
+//		System.out.printf("%f %f %f \n",10.1,20.1,30.2); // 실수
+//		System.out.printf("%c %c %c \n", 'A','B','C'); // 한 문자
+//		System.out.printf("%s %s %s \n","This is","String","TEST"); // 문자열
+//		System.out.printf("%d.%s : %d\n",1,"국어",100); 
+		
+		System.out.println("HELLOW"); 
+		System.out.println("HELLOW"); 
+		System.out.println("HELLOW"); 
+
+		System.out.println("I SAY \"HELLO\" "); 
+			
+	}
+
+}
