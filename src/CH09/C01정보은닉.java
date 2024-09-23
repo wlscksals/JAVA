2992758b499211a8f889db6d6bac05a31242b0ba
junyoung6 <rlrpqkd12@naver.com>
2024-09-23 10:17:51 +0900

    .

--- src/CH09/C01정보은닉.java
+++ src/CH09/C01정보은닉.java
@@ -4,31 +4,46 @@ class C01Person{
 	String name;
 	private int age;
 	String addr;
-	
-	C01Person(String name,int age,String addr){
-		this.name = name;
-		this.age = age;
-		this.addr = addr ;
+
+	@Override
+	public String toString() {
+		return "C01Person [name=" + name + ", age=" + age + ", addr=" + addr + "]";
 	}
 	
-	//getter and setter
-	public int getAge() {
-		return this.age;
-	}
-	public String getAdrr() {
-		return this.addr;
-	}
+
+	//생성자 함수 우클릭 
+//	C01Person(String name,int age,String addr){
+//		this.name = name;
+//		this.age = age;
+//		this.addr = addr ;
+//	}
+	
+	//getter and setter 
+//	public int getAge() {
+//		return this.age;
+//	}
+//	public String getAdrr() {
+//		return this.addr;
+//	}
+//	public String getname() {
+//		return this.name;
+//	}
+//	
+//	
+//	
+//	
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public void setAge(int age) {
+//		this.age = age;
+//	}
+//	
+//	public void setAddr(String addr) {
+//		this.addr = addr;
+//	}
 	
 	
-	
-	
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
-	public void setAddr(String addr) {
-		this.addr = addr;
-	}
 }
 
 public class C01정보은닉 {
@@ -40,10 +55,10 @@ public class C01정보은닉 {
 		// public 모든 클래스에서 접근 가능
 		// protected :
 		
-		C01Person hong = new C01Person("홍길동",50,"대구");
-		// hong. age가 private로 인해 안보임
-		int age = hong.getAge();
-		hong.setAddr("울산");
+//		C01Person hong = new C01Person("홍길동",50,"대구");
+//		// hong. age가 private로 인해 안보임
+//		int age = hong.getAge();
+//		hong.setAddr("울산");
 	}
 
 }
--- src/CH09/C02캡슐화.java
+++ src/CH09/C02캡슐화.java
@@ -1,10 +1,35 @@
 package CH09;
 
+ class Engine{
+	 private void 흡입() {System.out.println("외부로부터 공기를 빨아들인다.");}
+	 private void 압축() {System.out.println("가둔 공기를 압축시킨다.");}
+	 private void 폭발() {System.out.println("일정 수준압축되면 폭발!");}
+	 private void 배기() {System.out.println("가스를 외부로 보낸다.");}
+	 
+	 public void EngineStart() {
+		 흡입();
+		 압축();
+		 폭발();
+		 배기();
+	 }
+ 
+ }
+class Car{
+	Engine engine;
+	Car(){
+		engine = new Engine();
+	}
+	void run() {
+		engine.EngineStart();
+	}
+}
 public class C02캡슐화 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Car myCar = new Car();
+		myCar.run();
+		
 	}
 
 }
